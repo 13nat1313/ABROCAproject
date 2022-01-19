1d92bf1bfc3fef5a9ca2711164b741a83e785229
nswinger <nat.swinger@gmail.com>
2022-01-19 05:23:02 -0800

    Bug Fixes
    Fix to logistic regression CV
    
    Tests for covariate mean difference and label proportion difference

--- ABROCA.py
+++ ABROCA.py
@@ -37,21 +37,22 @@ def compute_abroca(fpr_0, tpr_0, fpr_1, tpr_1, n_grid=10000, lb=0, ub=1, limit=1
     return slice
 
 
-def simulate(subgroup_n=[10000, 10000], mu_0=[0, 0], mu_1=[0, 0], sigma_0=[1, 1], sigma_1=[1, 1],
-             rounding_thresh=[.5, .5], eta_sd=[0.1, 0.1], eta_mean=[0, 0], sigma_scale_factor=1, mu_scale_factor=0):
+def simulate(subgroup_n=[10000, 10000], mu_0=[0, 0], mu_1=[0, 0], sigma_0=[1, 1], sigma_1=[1, 1], eta_sd=[.1, .1], eta_mean=[0, 0], sigma_scale_factor=1, mu_scale_factor=0, label_prop=False):
 
     assert len(mu_1) == len(mu_0) == len(sigma_0) == len(sigma_1), 'num of covariates not consistent'
-    assert len(subgroup_n) == len(rounding_thresh) == len(eta_sd) == len(eta_mean), 'num of groups not consistent'
-
-    mu = [mu_0, np.dot(mu_1, mu_scale_factor)]
+    assert len(subgroup_n) == len(eta_sd) == len(eta_mean), 'num of groups not consistent'
+    if label_prop:
+        mu_1 = [mu_1[0]+mu_scale_factor, mu_1[1]+mu_scale_factor]
+    else:
+        mu_1 = [mu_1[0]+mu_scale_factor, mu_1[1]-mu_scale_factor]
+    mu = [np.array(mu_0), np.array(mu_1)]
     sigma = [np.diag(sigma_0), np.diag(np.dot(sigma_1, sigma_scale_factor))]
-
     X = np.array([])
     y = np.array([])
 
     for i in range(len(subgroup_n)):
         n_i = subgroup_n[i]
-        ax = np.random.multivariate_normal(mu[i], sigma[i], size=n_i)
+        ax = np.random.multivariate_normal(mu[i], sigma[i], size=n_i)     
         X = np.append(X.reshape((-1, len(mu_0))), ax, axis=0)
         aeta = np.random.normal(eta_mean[i], eta_sd[i], n_i).reshape((-1, 1))
         theta = np.ones((1, len(mu_0)))
@@ -62,7 +63,7 @@ def simulate(subgroup_n=[10000, 10000], mu_0=[0, 0], mu_1=[0, 0], sigma_0=[1, 1]
 
 
 def ABROCAvs_plot(plot_type, vs, r=10, s=0, n=[10000, 10000]):
-    """plot_type options are 'default', 'sample_size', 'label_dist', 'cov_means', 'label_noise', 'obs_noise'"""
+    """plot_type options are 'default', 'sample_size', 'label_dist', 'cov_means', 'obs_noise'"""
     np.random.seed(s)
     avg_abrocas = None
 
@@ -72,13 +73,11 @@ def ABROCAvs_plot(plot_type, vs, r=10, s=0, n=[10000, 10000]):
             if plot_type == 'sample_size':
                 X, y = simulate(subgroup_n=[int(round(sum(n)*e)), int(round(sum(n)*(1-e)))])
             elif plot_type == 'label_dist':
-                X, y = simulate(subgroup_n=n)
+                X, y = simulate(subgroup_n=n, mu_scale_factor=e, label_prop=True)
             elif plot_type == 'cov_means':
                 X, y = simulate(subgroup_n=n, mu_scale_factor=e)
-            elif plot_type == 'label_noise':
-                X, y = simulate(subgroup_n=n)
             elif plot_type == 'obs_noise':
-                X, y = simulate(subgroup_n=n, eta_sd=[0,e])
+                X, y = simulate(subgroup_n=n, eta_sd=[0.1,e])
             else:
                 X, y = simulate(subgroup_n=n)
 
@@ -87,14 +86,15 @@ def ABROCAvs_plot(plot_type, vs, r=10, s=0, n=[10000, 10000]):
 
             X_train = np.append(X_train_0, X_train_1, axis=0)
             y_train = np.append(y_train_0, y_train_1, axis=0)
-
+            
             if len(set(y_train)) == 1:
                 continue
 
             # regressor = RandomForestRegressor(n_estimators = 10, random_state=0)
             # regressor = LogisticRegression()
             regressor = LogisticRegressionCV(cv=5, random_state=0)
-            regressor.fit(X_train, y_train)
+            perm = np.random.permutation(len(y_train))
+            regressor.fit(X_train[perm], y_train[perm])
 
             y_pred_0 = regressor.predict(X_test_0)
             y_pred_1 = regressor.predict(X_test_1)
@@ -116,5 +116,11 @@ def ABROCAvs_plot(plot_type, vs, r=10, s=0, n=[10000, 10000]):
             avg_abrocas = np.append(avg_abrocas, np.array(abrocas).reshape((-1, 1)), axis=1)
 
     errors = np.nanstd(avg_abrocas, axis=1).ravel()
+    errors = np.array([i/np.sqrt(r) for i in errors])
     avg_abrocas = np.nanmean(avg_abrocas, axis=1).ravel()
+
     return avg_abrocas, vs, errors
+
+if __name__ == '__main__':
+    avg_abrocas, eta_sds, errors = ABROCAvs_plot('obs_noise', [10 ** i for i in np.arange(-3, 3, 0.2)], s=13,
+                                                        n=[5000, 5000])
