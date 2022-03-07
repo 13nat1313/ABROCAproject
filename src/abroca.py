8dd7ba95b78f66baadc78fcf7c0f8db5aba2b8e0
jpgard <joshua.patrick.gardner@gmail.com>
2022-03-07 15:55:22 -0800

    bug fixes in compute_abroca()

--- src/abroca.py
+++ src/abroca.py
@@ -12,10 +12,8 @@ def interpolate_roc_fun(fpr, tpr, n_grid):
     return x_new, y_new
 
 
-def compute_abroca(fpr_0, tpr_0, fpr_1, tpr_1, n_grid=10000, lb=0, ub=1,
-                   limit=1000):
-    """https://github.com/VaibhavKaushik3220/abroca/blob/main/abroca/compute_abroca.py"""
-    # Compute the value of the abroca statistic.
+def compute_abroca(fpr_0, tpr_0, fpr_1, tpr_1, n_grid=10000, lb=0., ub=1.):
+    """Compute the value of the abroca statistic."""
 
     # compare minority to majority class; accumulate absolute difference btw ROC curves to slicing statistic
     majority_roc_x, majority_roc_y = interpolate_roc_fun(fpr_0, tpr_0, n_grid)
@@ -24,10 +22,10 @@ def compute_abroca(fpr_0, tpr_0, fpr_1, tpr_1, n_grid=10000, lb=0, ub=1,
     # use function approximation to compute slice statistic via piecewise linear function
     assert list(majority_roc_x) == list(
         minority_roc_x), "Majority and minority FPR are different"
-    f1 = interpolate.interp1d(x=majority_roc_x,
+    between_roc_dist = interpolate.interp1d(x=majority_roc_x,
                               y=(majority_roc_y - minority_roc_y))
-    f2 = lambda x, acc: abs(f1(x))
-    slice, _ = integrate.quad(f2, lb, ub, limit)
+    abs_between_roc_dist = lambda x: abs(between_roc_dist(x))
+    slice, _ = integrate.quad(func=abs_between_roc_dist, a=lb, b=ub)
 
     return slice
 
