5a269a9dcb953c6e9db47152293100268e01470d
nswinger <nat.swinger@gmail.com>
2022-03-09 05:05:37 -0800

    Added Documentation

--- src/simulation.py
+++ src/simulation.py
@@ -3,7 +3,51 @@ from scipy.special import expit
 
 
 class SimulationParams:
-    def __init__(self, d: int = 2, n: int = 10000, p_0: float = 0.5, eta_sd: np.ndarray = np.full(2, 0.1), eta_mean: np.ndarray = np.zeros(2), mu_0: np.ndarray = None, mu_1: np.ndarray = None, sigma_0: np.ndarray = None, sigma_1: np.ndarray = None, theta_0: np.ndarray = None, theta_1: np.ndarray = None, sigma_scale_factor: float = 1, mu_change: float = 0, orthog_to_boundary: bool = False):
+    """Object storing default data simulation parameters, e.g., number of covariates, subgroup sizes, covariate means, etc.
+
+    Attributes:
+        n (int): Population size.
+        d (int): Number of covariates.
+        p_0 (float): Size of subgroup 0 as proportion of population size.
+        eta_sd (np.ndarray[float]): Standard deviation of error (η) for each subgroup.
+        eta_mean (np.ndarray[float]): Mean of error (η) for each subgroup.
+        mu_0 (np.ndarray[float]): Mean (μ) of each covariate of subgroup 0.
+        mu_1 (np.ndarray[float]): Means (μ) of each covariate of subgroup 1.
+        sigma_0 (np.ndarray[float]): Standard deviations (σ) of each covariate of subgroup 0.
+        sigma_1 (np.ndarray[float]): Standard deviations (σ) of each covariate of subgroup 1.
+        theta_0 (np.ndarray[float]): Weights (θ) of each covariate of subgroup 0.
+        theta_1 (np.ndarray[float]): Weights (θ) of each covariate of subgroup 1.
+        sigma_scale_factor (float): Value used to change 'sigma_1' using scalar multiplication ('sigma_scale_factor' * 'sigma_1').
+        mu_change (float): Value used to change means of covariates of subgroup 1 in relation to classification boundary
+        orthog_to_boundary (bool): If True, any change to 'mu_1' will occur othogonally to classification boundary. If False, change will occur parallel to boundary.
+    """
+    def __init__(self, d: int = 2, n: int = 10000, p_0: float = 0.5, eta_sd: np.ndarray[float] = np.full(2, 0.1),
+                 eta_mean: np.ndarray[float] = np.zeros(2), mu_0: np.ndarray[float] = None, mu_1: np.ndarray[float] = None,
+                 sigma_0: np.ndarray[float] = None, sigma_1: np.ndarray[float] = None, theta_0: np.ndarray[float] = None,
+                 theta_1: np.ndarray[float] = None, sigma_scale_factor: float = 1, mu_change: float = 0,
+                 orthog_to_boundary: bool = False):
+        """Initializes SimulationParams with given parameters.
+
+        Args:
+            d: Number of covariates.
+            n: Population size.
+            p_0: Size of subgroup 0 as proportion of population size.
+            eta_sd: Standard deviation of error (η) for each subgroup.
+            eta_mean: Mean of error (η) for each subgroup.
+            mu_0: Mean (μ) of each covariate of subgroup 0.
+            mu_1: Means (μ) of each covariate of subgroup 1.
+            sigma_0: Standard deviations (σ) of each covariate of subgroup 0.
+            sigma_1: Standard deviations (σ) of each covariate of subgroup 1.
+            theta_0: Weights (θ) of each covariate of subgroup 0.
+            theta_1: Weights (θ) of each covariate of subgroup 1.
+            sigma_scale_factor: Value used to change 'sigma_1' using scalar multiplication ('sigma_scale_factor' * 'sigma_1').
+            mu_change: Value used to change means of covariates of subgroup 1 in relation to classification boundary
+            orthog_to_boundary: If True, any change to 'mu_1' will occur othogonally to classification boundary. If False, change will occur parallel to boundary.
+
+        Raises:
+            AssertionError: If length of any of 'mu_0', 'mu_1', 'sigma_0', 'sigma_1', 'theta_0', or 'theta_1' do not equal 'd'.
+            AssertionError: If length of either of 'eta_sd' or 'eta_mean' does not equal 2.
+        """
         self.d = d
         self.n = n
         self.p_0 = p_0
@@ -35,28 +79,36 @@ class SimulationParams:
             v = np.append(v, z)
             self.mu_1 = np.array(self.mu_1) + (v * self.mu_change)
         self.sigma_0 = np.diag(self.sigma_0)
-        self.sigma_1 = np.diag(np.dot(self.sigma_1, self.sigma_scale_factor))
+        self.sigma_1 = np.diag(self.sigma_scale_factor*np.array(self.sigma_1))
         self.theta_0 = np.array([self.theta_0])
         self.theta_1 = np.array([self.theta_1])
 
 
-def simulate(dflts: SimulationParams = None):
-    if dflts is None:
-        dflts = SimulationParams()
+def simulate(defaults: SimulationParams = None) -> (np.ndarray, np.ndarray):
+    """Generate simulated data and matching labels.
 
-    mu = [dflts.mu_0, dflts.mu_1]
-    sigma = [dflts.sigma_0, dflts.sigma_1]
-    theta = [dflts.theta_0, dflts.theta_1]
+    Args:
+        defaults: Object storing default data simulation parameters, e.g., number of covariates, subgroup sizes, covariate means, etc.
+
+    Returns:
+        A tuple (X, y), where 'X' is an array of data of shape (NxM) where N equals population size and M equals number of covariates and 'y' is an array binary labels (coresponding to 'X') of shape (Nx1) where N equals population size.
+    """
+    if defaults is None:
+        defaults = SimulationParams()
+
+    mu = [defaults.mu_0, defaults.mu_1]
+    sigma = [defaults.sigma_0, defaults.sigma_1]
+    theta = [defaults.theta_0, defaults.theta_1]
 
     X = np.array([])
     y = np.array([])
 
-    subgroup_n = [int(round(dflts.n * dflts.p_0)), int(round(dflts.n * (1 - dflts.p_0)))]
+    subgroup_n = [int(round(defaults.n * defaults.p_0)), int(round(defaults.n * (1 - defaults.p_0)))]
     for i in range(2):
         n_i = subgroup_n[i]
         ax = np.random.multivariate_normal(mu[i], sigma[i], size=n_i)
-        X = np.append(X.reshape((-1, dflts.d)), ax, axis=0)
-        aeta = np.random.normal(dflts.eta_mean[i], dflts.eta_sd[i], n_i).reshape((-1, 1))
+        X = np.append(X.reshape((-1, defaults.d)), ax, axis=0)
+        aeta = np.random.normal(defaults.eta_mean[i], defaults.eta_sd[i], n_i).reshape((-1, 1))
         ap = expit(np.sum(np.append(theta[i] * ax, aeta, axis=1), axis=1))
         ay = np.less_equal(np.random.uniform(size=len(ap)), ap)
         y = np.append(y, ay, axis=0)
--- src/visualization.py
+++ src/visualization.py
@@ -12,6 +12,17 @@ from src.simulation import simulate, SimulationParams
 
 
 def regress(X, y, n, p_0):
+    """Performs an unregularized logistic regression using given data and labels to find ABROCA and other related statistics.
+
+    Args:
+        X (np.ndarray): Data of shape (NxM) where N equals population size and M equals number of covariates.
+        y (np.ndarray): Binary labels (coresponding to 'X') of shape (Nx1) where N equals population size.
+        n (int): Population size.
+        p_0 (float): Size of subgroup 0 as proportion of population size.
+
+    Returns:
+        If possible to calculate all statistics, returns tuple (fpr_0, tpr_0, fpr_1, tpr_1, g0auc, g1auc, abroca), where 'fpr_0' is regressor false positive rate for subgroup 0, 'tpr_0' is regressor true positive rate for subgroup 0, 'fpr_1' is regressor false positive rate for subgroup 1, 'tpr_1' is regressor true positive rate for subgroup 1, 'g0auc' is regressor AUC for subgroup 0, 'g1auc' is regressor AUC for subgroup 1, and 'abroca' is ABROCA value for the two subgroups. If not possible, returns None.
+    """
     n_0 = int(round(n * p_0))
     X_train_0, X_test_0, y_train_0, y_test_0 = train_test_split(X[:n_0], y[:n_0], test_size=0.2, random_state=0)
     X_train_1, X_test_1, y_train_1, y_test_1 = train_test_split(X[n_0:], y[n_0:], test_size=0.2, random_state=0)
@@ -21,7 +32,6 @@ def regress(X, y, n, p_0):
 
     X_train = np.append(X_train_0, X_train_1, axis=0)
     y_train = np.append(y_train_0, y_train_1, axis=0)
-    y_test = np.append(y_test_0, y_test_1, axis=0)
 
     if len(set(y_train)) == 1:
         return
@@ -34,7 +44,6 @@ def regress(X, y, n, p_0):
 
     y_pred_0 = regressor.predict(X_test_0)
     y_pred_1 = regressor.predict(X_test_1)
-    y_pred = np.append(y_pred_0, y_pred_1, axis=0)
 
     fpr_0, tpr_0, _ = roc_curve(y_test_0, y_pred_0)
     fpr_1, tpr_1, _ = roc_curve(y_test_1, y_pred_1)
@@ -47,7 +56,18 @@ def regress(X, y, n, p_0):
 
 
 def ABROCAvs_plot(plot_type, versus, r=10, s=0, **kwargs):
-    """plot_type options are 'n', 'p_0', 'mu_orthogonal', 'mu_parallel', 'obs_noise', 'theta_diff'"""
+    """Generates mean ABROCA values and standard errors that are necessary for creating 'ABROCA vs Change in Simulation Parameter' plots.
+
+    Args:
+        plot_type (str): Chooses which simulation parameter to manipulate. Options are 'n', 'p_0', 'mu_orthogonal', 'mu_parallel', 'obs_noise', 'theta_diff'.
+        versus (np.ndarray[float]): Varied values of some simulation parameter which is used in calculation of ABROCA.
+        r (int): Number of trials of ABROCA calculation, each with a new randomly simulated dataset based on same simulation parameters.
+        s (int): Random seed.
+        **kwargs: Passed to SimulationParams.
+
+    Returns:
+        A tuple (avg_abrocas, errors), where 'avg_abrocas' is an array of values for each set of simulation parameters, with each value being the mean ABROCA score over 'r' trials, and 'errors' is a similar array, but with standard errors instead of means.
+    """
 
     np.random.seed(s)
     avg_abrocas = None
@@ -91,7 +111,19 @@ def ABROCAvs_plot(plot_type, versus, r=10, s=0, **kwargs):
 
 
 def two_way_plot(big_x, big_y, plot_type, s=0, figsize=(20, 20), **kwargs):
-    """options = 'p_0 vs mu_orthogonal', 'p_0 vs mu_parallel', 'p_0 vs obs_noise', 'p_0 vs theta_diff', 'p_0 vs n', 'mu_orthogonal vs obs_noise', 'mu_orthogonal vs theta_diff', 'mu_orthogonal vs n', 'mu_parallel vs obs_noise', 'mu_parallel vs theta_diff', 'mu_parallel vs n', 'obs_noise vs theta_diff', 'obs_noise vs n', 'theta_diff vs n'"""
+    """Generates a set of two-curve ROC plots, varying one simulation parameter along the big x-axis and another along the big y-axis.
+
+    Args:
+        big_x (np.ndarray[float]): Varied values of some simulation parameter which is used in calculation of ABROCA.
+        big_y (np.ndarray[float]): Varied values of some simulation parameter which is used in calculation of ABROCA.
+        plot_type (str): Chooses which simulation parameter to manipulate. Options are 'p_0 vs mu_orthogonal', 'p_0 vs mu_parallel', 'p_0 vs obs_noise', 'p_0 vs theta_diff', 'p_0 vs n', 'mu_orthogonal vs obs_noise', 'mu_orthogonal vs theta_diff', 'mu_orthogonal vs n', 'mu_parallel vs obs_noise', 'mu_parallel vs theta_diff', 'mu_parallel vs n', 'obs_noise vs theta_diff', 'obs_noise vs n', and 'theta_diff vs n'.
+        s (int): Random seed.
+        figsize (tuple[int]): Size of matplotlib.pyplot figure.
+        **kwargs: Passed to SimulationParams.
+
+    Returns:
+        fig (Figure): A matplotlib Figure with a set of two-curve ROC plots
+    """
 
     np.random.seed(s)
     fig, axs = plt.subplots(len(big_x), len(big_y), figsize=figsize)
@@ -99,7 +131,8 @@ def two_way_plot(big_x, big_y, plot_type, s=0, figsize=(20, 20), **kwargs):
     for row, a_x in enumerate(big_x):
         for col, a_y in enumerate(big_y):
             if plot_type == 'p_0 vs mu_orthogonal':
-                kwargs['p_0'], kwargs['mu_change'], kwargs['orthog_to_boundary'], xaxis, yaxis = a_x, a_y, True, 'p_0', 'mu_orthogonal'
+                kwargs['p_0'], kwargs['mu_change'], kwargs[
+                    'orthog_to_boundary'], xaxis, yaxis = a_x, a_y, True, 'p_0', 'mu_orthogonal'
             elif plot_type == 'p_0 vs mu_parallel':
                 kwargs['p_0'], kwargs['mu_change'], xaxis, yaxis = a_x, a_y, 'p_0', 'mu_parallel'
             elif plot_type == 'p_0 vs obs_noise':
@@ -110,12 +143,13 @@ def two_way_plot(big_x, big_y, plot_type, s=0, figsize=(20, 20), **kwargs):
                 kwargs['n'], kwargs['p_0'], xaxis, yaxis = a_y, a_x, 'p_0', 'n'
             elif plot_type == 'mu_orthogonal vs obs_noise':
                 kwargs['mu_change'], kwargs['orthog_to_boundary'], kwargs['eta_sd'], xaxis, yaxis = a_x, True, [0.1,
-                                                                                  a_y], 'mu_orthogonal', 'obs_noise'
+                                                                                                                a_y], 'mu_orthogonal', 'obs_noise'
             elif plot_type == 'mu_orthogonal vs theta_diff':
                 kwargs['mu_change'], kwargs['orthog_to_boundary'], kwargs['theta_1'], xaxis, yaxis = a_x, True, [1,
-                                                                                   1 * a_y], 'mu_orthogonal', 'theta_diff'
+                                                                                                                 1 * a_y], 'mu_orthogonal', 'theta_diff'
             elif plot_type == 'mu_orthogonal vs n':
-                kwargs['n'], kwargs['mu_change'], kwargs['orthog_to_boundary'], xaxis, yaxis = a_y, a_x, True, 'mu_orthogonal', 'n'
+                kwargs['n'], kwargs['mu_change'], kwargs[
+                    'orthog_to_boundary'], xaxis, yaxis = a_y, a_x, True, 'mu_orthogonal', 'n'
             elif plot_type == 'mu_parallel vs obs_noise':
                 kwargs['mu_change'], kwargs['eta_sd'], xaxis, yaxis = a_x, [0.1, a_y], 'mu_parallel', 'obs_noise'
             elif plot_type == 'mu_parallel vs theta_diff':
@@ -148,15 +182,36 @@ def two_way_plot(big_x, big_y, plot_type, s=0, figsize=(20, 20), **kwargs):
 
 
 def visualize_data_3d(plot_vars):
+    """Helper function that aids in generation of 2 figures to help display simulated data as a scatter plot.
+
+    Args:
+        plot_vars (tuple[list]): A tuple created by visualize_data with lists of the first 3 covariates of data in plottable format.
+
+    Returns:
+        A tuple (fig0, fig1), where 'fig0' is a 3D scatter plot of subgroup 0 simulated data and 'fig1' is a 3D scatter plot of subgroup 1 data.
+    """
     colors = ['0 label' for _ in plot_vars[0]] + ['1 label' for _ in plot_vars[3]]
-    fig0 = px.scatter_3d(x=plot_vars[0]+plot_vars[3],y=plot_vars[1]+plot_vars[4],z=plot_vars[2]+plot_vars[5], color=colors, size=[.02 for _ in plot_vars[0]+plot_vars[3]], title='Group 0')
+    fig0 = px.scatter_3d(x=plot_vars[0] + plot_vars[3], y=plot_vars[1] + plot_vars[4], z=plot_vars[2] + plot_vars[5],
+                         color=colors, size=[.02 for _ in plot_vars[0] + plot_vars[3]], title='Group 0')
 
     colors = ['0 label' for _ in plot_vars[6]] + ['1 label' for _ in plot_vars[9]]
-    fig1 = px.scatter_3d(x=plot_vars[6]+plot_vars[9],y=plot_vars[7]+plot_vars[10],z=plot_vars[8]+plot_vars[11], color=colors, size=[1 for _ in plot_vars[6]+plot_vars[9]], title='Group 1')
+    fig1 = px.scatter_3d(x=plot_vars[6] + plot_vars[9], y=plot_vars[7] + plot_vars[10], z=plot_vars[8] + plot_vars[11],
+                         color=colors, size=[1 for _ in plot_vars[6] + plot_vars[9]], title='Group 1')
     return fig0, fig1
 
 
 def visualize_data(point_size=.5, s=0, figsize=(20, 20), **kwargs):
+    """Generates either 1 or 2 figures to help display simulated data as a scatter plot.
+
+    Args:
+        point_size (float): Size of scatter plot points, passed as argument to plt.scatter.
+        s (int): Random seed.
+        figsize (tuple[int]): Size of matplotlib.pyplot figure.
+        **kwargs: Passed to SimulationParams
+
+    Returns:
+        If data is generated with 2 covariates, returns a matplotlib.pyplot figure with 2 scatter plots (group 0 and 1 data), and 2 ROC plots (group 0 and group 1). If more than 2 covariates, returns the tuple returned by visualize_data_3d.
+    """
     dflts = SimulationParams(**kwargs)
     np.random.seed(s)
 
@@ -196,7 +251,10 @@ def visualize_data(point_size=.5, s=0, figsize=(20, 20), **kwargs):
         plot_z0_1 = [a for a, b in zip(X_train_0[:, 2], y_train_0) if b == 1]
         plot_z1_0 = [a for a, b in zip(X_train_1[:, 2], y_train_1) if b == 0]
         plot_z1_1 = [a for a, b in zip(X_train_1[:, 2], y_train_1) if b == 1]
-        plot_vars = (plot_x0_0, plot_y0_0, plot_z0_0, plot_x0_1, plot_y0_1, plot_z0_1, plot_x1_0, plot_y1_0, plot_z1_0, plot_x1_1, plot_y1_1, plot_z1_1)
+        plot_vars = (
+            plot_x0_0, plot_y0_0, plot_z0_0, plot_x0_1, plot_y0_1, plot_z0_1, plot_x1_0, plot_y1_0, plot_z1_0,
+            plot_x1_1,
+            plot_y1_1, plot_z1_1)
         return visualize_data_3d(plot_vars)
     else:
         fig, axs = plt.subplots(2, 2, figsize=figsize)
@@ -206,13 +264,15 @@ def visualize_data(point_size=.5, s=0, figsize=(20, 20), **kwargs):
         axs[1, 1].set_title(f'Group 1 ROC\n auc = {g1auc}')
 
         axs[0, 0].scatter(plot_x0_0, plot_y0_0, color='blue', s=point_size, label=f'0 label ({len(plot_x0_0)} points)')
-        axs[0, 0].scatter(plot_x0_1, plot_y0_1, color='orange', s=point_size, label=f'1 label ({len(plot_x0_1)} points)')
+        axs[0, 0].scatter(plot_x0_1, plot_y0_1, color='orange', s=point_size,
+                          label=f'1 label ({len(plot_x0_1)} points)')
         axs[0, 0].legend()
         axs[0, 0].set_title('Group 0')
 
         axs[0, 1].scatter(plot_x1_0, plot_y1_0, color='blue', s=point_size, label=f'0 label ({len(plot_x1_0)} points)')
 
-        axs[0, 1].scatter(plot_x1_1, plot_y1_1, color='orange', s=point_size, label=f'1 label ({len(plot_x1_1)} points)')
+        axs[0, 1].scatter(plot_x1_1, plot_y1_1, color='orange', s=point_size,
+                          label=f'1 label ({len(plot_x1_1)} points)')
         axs[0, 1].legend()
         axs[0, 1].set_title('Group 1')
 
